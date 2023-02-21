de8c60677e5f57d7830dd72e49abe9c83e2f94dc
dan <djosephhenri@gmail.com>
2023-02-21 17:57:04 +0900

    added submit function

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
+const bodyParser = require('body-parser')
 const express = require('express')
 const nodemailer = require('nodemailer')
 const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`nodemailerProject is listening at http://localhost:${port}`)
@@ -26,17 +28,32 @@ let transporter = nodemailer.createTransport({
   }
 });
 
-let mailOptions = {
-  from: 'djosephhenri@gmail.com',
-  to: 'djosephhenri@gmail.com',
-  subject: 'Nodemailer Project',
-  text: 'Hi from your nodemailer project'
-};
+// let mailOptions = {
+  // from: 'djosephhenri@gmail.com',
+  // to: 'maino.jess@gmail.com',
+  // subject: 'testing testing',
+  // text: 'testing'
+// };
 
-/* transporter.sendMail(mailOptions, function(err, data) { */
-/*   if (err) { */
-/*     console.log("Error " + err); */
-/*   } else { */
-/*     console.log("Email sent successfully"); */
-/*   } */
-/* }); */
+app.post('/', function (req, res) {
+  let mailOptions = {
+    from: 'djosephhenri@gmail.com',
+    to: req.body.email,
+    subject: 'tozen mailer test',
+    text: 'this is an automated test of the tozen mailer' 
+  }
+  sendEmail(mailOptions)
+  console.log('email has been sent')
+  res.sendFile(path.join(__dirname, 'static/postsubmit.html'))
+  console.log(req.body)
+})
+
+function sendEmail(mailOptions) {
+  transporter.sendMail(mailOptions, function(err, data) {
+    if (err) {
+      console.log("Error " + err)
+    } else {
+      console.log("Email sent successfully")
+    }
+  })
+}
