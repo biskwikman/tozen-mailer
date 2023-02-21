a9ab2127f2d673ff369ad0f2f2053cb557dcce0a
Dan Henri <djosephhenri@gmail.com>
2023-02-21 22:27:38 +0900

    prepared for gcloud deployment

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const express = require('express')
 const nodemailer = require('nodemailer')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
-  console.log(`nodemailerProject is listening at http://localhost:${port}`)
+  console.log(`tozen-mailer is listening at http://localhost:${port}`)
 });
 
 app.get("/", (req, res) => {
@@ -21,26 +21,20 @@ let transporter = nodemailer.createTransport({
   auth: {
     type: 'OAuth2',
     user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
     clientId: process.env.OAUTH_CLIENTID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   }
 });
-
-// let mailOptions = {
-  // from: 'djosephhenri@gmail.com',
-  // to: 'maino.jess@gmail.com',
-  // subject: 'testing testing',
-  // text: 'testing'
-// };
 
 app.post('/', function (req, res) {
   let mailOptions = {
     from: 'djosephhenri@gmail.com',
     to: req.body.email,
-    subject: 'tozen mailer test',
-    text: 'this is an automated test of the tozen mailer' 
+    subject: 'tozen mailer test.',
+    text: 'this is an automated test of the tozen mailer. ' + '\n' +
+      'first name: ' + req.body.fname[0] + ', ' + req.body.fname[1] + '\n' +
+      'last name: ' + req.body.lname[0] + ', ' + req.body.lname[1]
   }
   sendEmail(mailOptions)
   console.log('email has been sent')
